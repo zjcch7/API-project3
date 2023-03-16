451cae16fe778e2be5ef37034d1488f57b8e9146
z c <zjcch7@umsystem.edu>
2023-03-16 16:17:10 -0500

    mode changes

--- apiTest.py
+++ apiTest.py
@@ -3,15 +3,24 @@ import requests
 import pygal
 import lxml
 from pprint import pprint
-ts = TimeSeries(key='Y7P82MTGYSOW6CEX', output_format='pandas')
+while(True):
+    try:
+    
+        ts = TimeSeries(key='Y7P82MTGYSOW6CEX', output_format='pandas')
 
-data_daily, meta_data = ts.get_daily_adjusted(symbol=input('Please Enter The Symbol: '), outputsize='full')
+        data_daily, meta_data = ts.get_daily_adjusted(symbol=input('Please Enter The Symbol: '), outputsize='full')
 
-start_date = datetime.datetime(input('Enter Start Date (MUST BE YYYY, MM, DD): '))
-end_date = datetime.datetime(input('Enter End Date (MUST BE YYYY, MM, DD)'))
+        start_date = datetime.datetime(input('Enter Start Date (MUST BE YYYY, MM, DD): '))
+        end_date = datetime.datetime(input('Enter End Date (MUST BE YYYY, MM, DD)'))
 
-date_filter = data_daily[(data_daily.index > start_date) & (data_daily.index <= end_date)]
-date_filter = date_filter.sort_index(ascending=True)
+        date_filter = data_daily[(data_daily.index > start_date) & (data_daily.index <= end_date)]
+        date_filter = date_filter.sort_index(ascending=True)
 
 
-pprint(data.head(2))
+        pprint(data.head(2))
+
+    except:
+        while(True):
+            another_stock = input("Do you want to look at another stock? (y/n): ")
+            if (another_stock != "y"):
+            break
