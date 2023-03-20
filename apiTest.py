4e61370fa902dcd6e1661c7ad1253521130ed4b5
zjcch7 <82986889+zjcch7@users.noreply.github.com>
2023-03-20 10:23:48 -0500

    looping
    The looping works correctly when the program doesn't return anything but is infinite when results are delivered.

--- apiTest.py
+++ apiTest.py
@@ -7,67 +7,74 @@ from lxml import etree
 
 print("Stock Data Visualizer")
 print("----------------------")
-print(" ")
-symbol = input("Enter stock symbol: ")
-print(" ")
-print("Chart Types")
-print("-------------")
-print("1: Line")
-print("2: Bar")
-print(" ")
-chart_type = input("Enter chart type (1, 2): ")
-print(" ")
-print("Select Time Series of the chart you want to Generate")
-print("------------------------------------------------------")
-print("1: Intraday")
-print("2: Daily")
-print("3: Weekly")
-print("4: Monthly")
-print(" ")
-time_series = input("Enter time series function (1-4): ")
-if time_series == "1":
-    interval = input("Enter time interval (1min, 5min, 15min, 30min, or 60min): ")
-    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval={interval}&apikey=Y7P82MTGYSOW6CEX"
-elif time_series == "2":
-    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey=Y7P82MTGYSOW6CEX"
-elif time_series == "3":
-    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol={symbol}&apikey=Y7P82MTGYSOW6CEX"
-elif time_series == "4":
-    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={symbol}&apikey=Y7P82MTGYSOW6CEX"
+while(True):
+    try:
+        print(" ")
+        symbol = input("Enter stock symbol: ")
+        print(" ")
+        print("Chart Types")
+        print("-------------")
+        print("1: Line")
+        print("2: Bar")
+        print(" ")
+        chart_type = input("Enter chart type (1, 2): ")
+        print(" ")
+        print("Select Time Series of the chart you want to Generate")
+        print("------------------------------------------------------")
+        print("1: Intraday")
+        print("2: Daily")
+        print("3: Weekly")
+        print("4: Monthly")
+        print(" ")
+        time_series = input("Enter time series function (1-4): ")
+        if time_series == "1":
+            interval = input("Enter time interval (1min, 5min, 15min, 30min, or 60min): ")
+            url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval={interval}&apikey=Y7P82MTGYSOW6CEX"
+        elif time_series == "2":
+            url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey=Y7P82MTGYSOW6CEX"
+        elif time_series == "3":
+            url = f"https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol={symbol}&apikey=Y7P82MTGYSOW6CEX"
+        elif time_series == "4":
+            url = f"https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={symbol}&apikey=Y7P82MTGYSOW6CEX"
 
-start_date = input("Enter start date (YYYY-MM-DD): ")
-end_date = input("Enter end date (YYYY-MM-DD): ")
+        start_date = input("Enter start date (YYYY-MM-DD): ")
+        end_date = input("Enter end date (YYYY-MM-DD): ")
 
-response = requests.get(url)
-response.raise_for_status()
+        response = requests.get(url)
+        response.raise_for_status()
 
-json_data = response.json()
-json_key = list(json_data.keys())[1]
-data = json_data[json_key]
+        json_data = response.json()
+        json_key = list(json_data.keys())[1]
+        data = json_data[json_key]
 
-filtered_data = {}
-if time_series == "intraday":
-    for time_interval, values in data.items():
-        date = time_interval[:10]
-        if start_date <= date <= end_date:
-            filtered_date[time_interval] = values
-else:
-    for date, values in data.items():
-        if start_date <= date <= end_date:
-            filtered_data[date] = values
+        filtered_data = {}
+        if time_series == "intraday":
+            for time_interval, values in data.items():
+                date = time_interval[:10]
+                if start_date <= date <= end_date:
+                    filtered_date[time_interval] = values
+        else:
+            for date, values in data.items():
+                if start_date <= date <= end_date:
+                    filtered_data[date] = values
 
-sorted_data = sorted(filtered_data.items(), reverse=False)
+        sorted_data = sorted(filtered_data.items(), reverse=False)
 
-if chart_type == "1":
-    chart = pygal.Line(x_label_rotation=20, show_minor_x_labels=True)
-elif chart_type == "2":
-    chart = pygal.Bar(x_label_rotation=20, show_minor_x_labels=True)
+        if chart_type == "1":
+            chart = pygal.Line(x_label_rotation=20, show_minor_x_labels=True)
+        elif chart_type == "2":
+            chart = pygal.Bar(x_label_rotation=20, show_minor_x_labels=True)
 
 
-chart.title = f"{symbol} Stock: {start_date} to {end_date}"
-chart.x_labels = [date for date, value in sorted_data]
-chart.add("Open", [float(value["1. open"]) for date, value in sorted_data])
-chart.add("High", [float(value["2. high"])for date, value in sorted_data])
-chart.add("Low", [float(value["3. low"]) for date, value in sorted_data])
-chart.add("Close", [float(value["4. close"]) for date, value in sorted_data])
-chart.render_in_browser()
+        chart.title = f"{symbol} Stock: {start_date} to {end_date}"
+        chart.x_labels = [date for date, value in sorted_data]
+        chart.add("Open", [float(value["1. open"]) for date, value in sorted_data])
+        chart.add("High", [float(value["2. high"])for date, value in sorted_data])
+        chart.add("Low", [float(value["3. low"]) for date, value in sorted_data])
+        chart.add("Close", [float(value["4. close"]) for date, value in sorted_data])
+        chart.render_in_browser()
+       
+    except:
+        another_stock = input("Do you want to look at another stock? (y/n): ")
+        if (another_stock != "y"):
+            break
